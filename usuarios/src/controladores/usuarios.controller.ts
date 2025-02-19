096f7b5838aafce2fd712769678c92d1f3606754
Fr4ncx-04 <jose.1993.fran@gmail.com>
2025-02-18 18:53:09 -0600

    cambios 1

--- usuarios/src/controladores/usuarios.controller.ts
+++ usuarios/src/controladores/usuarios.controller.ts
@@ -1,24 +1,9 @@
-
-//obtener la fecha y hora actual
-const obtenerFechaHora = () => {
-    return new Date().toDateString();
-};
-
-
 //controlador para obtener la lista de usuarios
 export const getUsuarios = (req: any, res: any) => {
-    res.json({
-        message: "Lista de usuarios",
-        timestamp: obtenerFechaHora()
-    });
-};
-
-
-//controlador para obtener un usuario por el id
-export const getUsuariosById = (req: any, res: any) => {
-    const {id}= req.params;
-    res.json({
-        message: `Detalles del usuario por el Id: ${id}`,
-        timestamp: obtenerFechaHora()
-    });
+        const Usuarios = [
+            {id: "1", name: "Jose", email:"jose@gmail.com"},
+            {id: "2", name: "Francisco", email:"fran@gmail.com"},
+            {id: "3", name: "Jovanny", email:"jovanny@gmail.com"}
+        ];
+        res.json(Usuarios);
 };
--- usuarios/src/routes/usuarios.routes.ts
+++ usuarios/src/routes/usuarios.routes.ts
@@ -1,12 +1,9 @@
 import {Router} from "express";
-import {getUsuarios, getUsuariosById} from "../controladores/usuarios.controller"
+import {getUsuarios} from "../controladores/usuarios.controller"
 
 const router=Router();
 
 //ruta para obtener todos los usuarios
-router.get("/", getUsuarios);
-
-//ruta para obtener un usuario por el id
-router.get("/:id", getUsuariosById);
+router.get("/lista", getUsuarios);
 
 export default router;
